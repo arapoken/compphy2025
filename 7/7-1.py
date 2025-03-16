fe6b94d7df59e09bc519aa034c4a8f0d2195a4d3
arapoken <821076631@qq.com>
2025-03-16 18:54:04 +0800

    hmk7

--- 7/7-1.py
+++ 7/7-1.py
@@ -1,3 +1,46 @@
 # ------------------ Classic Damped Oscillator ------------------
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
+
+n = 1000
+h = 100/n
+t = np.linspace(0, 100, n, endpoint=False)
+k = 0.1
+w = 1.0
+delta = np.sqrt(4*w**2 - k**2)/2
+
+phi = np.zeros(n) # dphi/dt = chi
+chi = np.zeros(n) # dchi/dt = -k/chi - w**2*phi
+phi[0] = 1.0
+chi[0] = 0.0 # initial conditions
+
+# 4th order Runge-Kutta method
+for i in range(n-1):
+    kphi1 = chi[i]
+    kchi1 = -k*chi[i] - w**2*phi[i]
+    kphi2 = chi[i] + kchi1*h/2
+    kchi2 = -k*(chi[i] + kchi1*h/2) - w**2*(phi[i] + kphi1*h/2)
+    kphi3 = chi[i] + kchi2*h/2
+    kchi3 = -k*(chi[i] + kchi2*h/2) - w**2*(phi[i] + kphi2*h/2)
+    kphi4 = chi[i] + kchi3*h
+    kchi4 = -k*(chi[i] + kchi3*h) - w**2*(phi[i] + kphi3*h)
+    phi[i+1] = phi[i] + (kphi1 + 2*kphi2 + 2*kphi3 + kphi4)*h/6
+    chi[i+1] = chi[i] + (kchi1 + 2*kchi2 + 2*kchi3 + kchi4)*h/6
+
+# determine whether there is phi<0, which means the appearance of an oscillation term
+print('Is there any oscillation?:',(phi < 0).any())
+# after adjusting the parameters, we found that when k>=2, the oscillation term disappears
+
+# analytical solution
+y = np.exp(-k/2*t)*(phi[0] * np.cos((delta)*t) + (chi[0]+k*phi[0]/2)/delta * np.sin(delta*t))
+
+# plot the result
+plt.plot(t, phi, 'o', c='red', markersize = 3.0)
+plt.plot(t, y, 'b-')
+plt.xlabel('Time')
+plt.ylabel('Amplitude')
+plt.legend(['Numerical result', 'Analytical result'])
+plt.title('Classic Damped Oscillator')
+plt.grid()
+plt.show()
+
--- 7/7-2.py
+++ 7/7-2.py
@@ -2,6 +2,61 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(-7, 7, 100)
-n = [1, 2, 5, 10, 15]
+N = 250
+h = 7/N
+x = np.linspace(0, 7, N, endpoint=False)
+n = 10 # quantum number
 
+u = np.zeros(N) # du/dx = v
+v = np.zeros(N) # dv/dx = (x**2 - 2*n -1)*u
+
+if n % 2 == 0:
+    u[0] = 1.0
+    v[0] = 0.0 # even boundary conditions
+else:
+    u[0] = 0.0
+    v[0] = 1.0 # odd boundary conditions
+
+# 4th order Runge-Kutta method
+for i in range(N-1):
+    ku1 = v[i]
+    kv1 = (x[i]**2 - 2*n - 1)*u[i]
+    ku2 = v[i] + 0.5*h*kv1
+    kv2 = ((x[i] + 0.5*h)**2 - 2*n - 1)*(u[i] + 0.5*h*ku1)
+    ku3 = v[i] + 0.5*h*kv2
+    kv3 = ((x[i] + 0.5*h)**2 - 2*n - 1)*(u[i] + 0.5*h*ku2)
+    ku4 = v[i] + h*kv3
+    kv4 = ((x[i] + h)**2 - 2*n - 1)*(u[i] + h*ku3)
+    u[i+1] = u[i] + (ku1 + 2*ku2 + 2*ku3 + ku4)*h/6
+    v[i+1] = v[i] + (kv1 + 2*kv2 + 2*kv3 + kv4)*h/6
+
+x = np.hstack((-np.flip(x), x)) # x axis extension
+if n % 2 == 0:
+    u = np.hstack((np.flip(u), u)) # even extension
+else:
+    u = np.hstack((-np.flip(u), u)) # odd extension
+
+rho = 1 / np.sqrt(2*n+1-x[(2*n+1-x**2) > 0]**2) # classical oscillator probability density
+rho = rho / np.sqrt(np.sum(rho**2*h)) # normalization
+print(np.sum(rho**2*h)) # check the normalization
+
+# plot the quantum harmonic oscillator
+plt.plot(x, u, 'b-')
+plt.xlabel('x')
+plt.ylabel('u(x)')
+plt.title('Quantum Harmonic Oscillator (n='+str(n)+')')
+plt.grid()
+plt.show()
+
+# plot the comparison between quantum and classical harmonic oscillators
+plt.plot(x, np.square(u), 'b-')
+plt.plot(x[(2*n+1-x**2) > 0], rho, 'r-')
+plt.xlabel('x')
+plt.ylabel('Probability density')
+plt.legend(['Quantum situation','Classical situation'])
+plt.title('Harmonic Oscillator Probability (n='+str(n)+')')
+plt.grid()
+plt.show()
+
+# problem 1: the boundary(x=-7 and x=7) action of quantum harmonic oscillator seems odd
+# problem 2: x**2 > 2*n +1, invalid value for sqrt
